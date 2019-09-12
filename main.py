be22bda86994ab70d011e34cc9fd553ca83897e6
Thomas TONDEUR <thomas@altazion.com>
2019-09-12 16:55:35 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from classes.game import Person, Bcolors
+
+
+magic = [{"name": "Fire", "cost": 10, "dmg": 60},
+         {"name": "Ice", "cost": 10, "dmg": 60},
+         {"name": "Thunder", "cost": 10, "dmg": 60}]
+
+
+player = Person(460, 65, 60, 34, magic)
+
